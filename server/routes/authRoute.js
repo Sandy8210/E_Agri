aa1a412faca156397d5576a91de475647a995e2c
KGM <KGM@CASL6-37-165.kgisledu.com>
2024-05-29 12:43:30 +0530

    restart

--- server/routes/authRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { register, login } = require("../controllers/authController");
-
-const routes = express.Router();
-
-routes.post("/signup", register);
-routes.post("/signin", login);
-
-module.exports = routes;
